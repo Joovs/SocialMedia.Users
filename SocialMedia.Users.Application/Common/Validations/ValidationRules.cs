6a1e3f87a70c82a11908f98ede41b4bbcaede1c1
Joovs <jovannaescogar9@gmail.com>
2026-01-14 17:59:24 -0600

    add: create static methods for validation rules

---
+++ SocialMedia.Users.Application/Common/Validations/ValidationRules.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SocialMedia.Users.Application.Common.Validations;
+
+public static class ValidationRules
+{
+    public static bool IsValidId(int id)
+        => id > 0;
+
+    public static bool HasCorrectLength(string value, int maxLength)
+    {
+        if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+            return false;
+
+        return value.Length <= maxLength;
+    }
+
+    public static bool IsValidEmail(string email)
+        => Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+    public static bool IsValidPassword(string password)
+        => Regex.IsMatch(password,
+           @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$");
+}
