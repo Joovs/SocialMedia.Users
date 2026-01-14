4b57239d0da4e0994ccf47a3a28fa0df25118765
Joovs <jovannaescogar9@gmail.com>
2026-01-14 17:31:35 -0600

    add: model creation

---
+++ SocialMedia.Users.Domain/Entities/UserEntity/Models/UpdateProfile/UpdateProfileResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SocialMedia.Users.Domain.Entities.UserEntity.Models.UpdateProfile;
+
+public class UpdateProfileResponseModel
+{
+    public string Message { get; set; } = string.Empty;
+    public DateTime UpdatedAt { get; set; }
+}
--- SocialMedia.Users.Domain/Entities/UserEntity/User.cs
+++ SocialMedia.Users.Domain/Entities/UserEntity/User.cs
@@ -11,6 +11,7 @@ public class User
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public string Lastname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
