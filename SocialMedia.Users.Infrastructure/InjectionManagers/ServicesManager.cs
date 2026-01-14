22bf3d8cbd212ff7b0f85508e9c9afc0e64667d8
Joovs <jovannaescogar9@gmail.com>
2026-01-14 17:45:08 -0600

    add: hasher service dependency injection

---
+++ SocialMedia.Users.Infrastructure/InjectionManagers/ServicesManager.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SocialMedia.Users.Domain.Services.Hasher;
+using SocialMedia.Users.Infrastructure.Services.Hasher;
+
+namespace SocialMedia.Users.Infrastructure.InjectionManagers;
+
+public static class ServicesManager
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<IHasher, Hasher>();
+        return services;
+    }
+}
