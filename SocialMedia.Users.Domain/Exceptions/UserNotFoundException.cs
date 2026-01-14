64e855a9116f51d9197be62c1a14fdd71098a6bd
Joovs <jovannaescogar9@gmail.com>
2026-01-14 17:45:55 -0600

    add: create custom exceptions

---
+++ SocialMedia.Users.Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace SocialMedia.Users.Domain.Exceptions;
+
+public class UserNotFoundException : Exception
+{
+    public UserNotFoundException()
+        : base("The user was not found.") { }
+}
