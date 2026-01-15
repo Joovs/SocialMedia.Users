ae2665a04d59bf4751da9404bfbacb2a8410fc3a
Joovs <jovannaescogar9@gmail.com>
2026-01-14 18:00:29 -0600

    add: create request and response of the command

---
+++ SocialMedia.Users.Application/Commands/UpdateProfile/UpdateProfileCommandRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialMedia.Users.Application.Commands.UpdateProfile;
+
+public class UpdateProfileCommandRequest
+{
+    public required int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
