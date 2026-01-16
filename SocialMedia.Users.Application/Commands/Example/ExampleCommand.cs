0b925d35f73590631107c51f89bbd7261eb894f6
Joovs <jovannaescogar9@gmail.com>
2026-01-15 22:36:02 -0600

    refactor: remove example command

--- SocialMedia.Users.Application/Commands/Example/ExampleCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-using SocialMedia.Users.Application.Shared;
-
-namespace SocialMedia.Users.Application.Commands.Example;
-
-public sealed record ExampleCommand (int userId) : IRequest<Result<ExampleCommandResponse>>
-{
-}
--- SocialMedia.Users.Application/Commands/Example/ExampleCommandResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SocialMedia.Users.Application.Commands.Example;
-
-public class ExampleCommandResponse
-{
-    public int UserId { get; set; }
-    public string UserName { get; set; } = string.Empty;
-}
