48956fed9c675e57569319449dd6936b92a6e135
Joovs <jovannaescogar9@gmail.com>
2026-01-16 18:50:41 -0600

    add: Model SeeFollowers

---
+++ SocialMedia.Users.Domain/Entities/FollowsEntity/Models/SeeFollowers/SeeFollowersModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialMedia.Users.Domain.Entities.FollowsEntity.Models.SeeFollowers;
+
+public class SeeFollowersModel
+{
+    public required List<Follower> Followers { get; set; }
+}
+
+public class Follower
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+}
