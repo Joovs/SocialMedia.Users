a9ff1cab9fa06a9932606802293709e849a5a601
Joovs <jovannaescogar9@gmail.com>
2026-01-16 18:39:12 -0600

    add: Follows entity

---
+++ SocialMedia.Users.Domain/Entities/FollowsEntity/Follows.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialMedia.Users.Domain.Entities.FollowsEntity;
+
+public class Follows
+{
+    public Guid FollowerId { get; set; }
+    public Guid FollowingId { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
