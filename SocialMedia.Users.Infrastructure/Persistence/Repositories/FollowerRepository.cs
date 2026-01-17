68e262a9552a990c18fa9057316b43c7b24dd902
Joovs <jovannaescogar9@gmail.com>
2026-01-16 20:38:06 -0600

    add: class that implements the IFollowerRepository interface

---
+++ SocialMedia.Users.Infrastructure/Persistence/Repositories/FollowerRepository.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Core;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Users.Domain.Entities.FollowsEntity.Models.SeeFollowers;
+using SocialMedia.Users.Domain.Entities.FollowsEntity.Repositories;
+using SocialMedia.Users.Infrastructure.Persistence.Context;
+
+namespace SocialMedia.Users.Infrastructure.Persistence.Repositories;
+
+public class FollowerRepository : IFollowerRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public FollowerRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> UserExists(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.Users.AsNoTracking().AnyAsync(u => u.Id == id);
+    }
+
+    public async Task<List<Follower>> SeeFollowers(Guid id, CancellationToken cancellationToken)
+    {
+        List<Follower> followers = await (from u in _context.Users.AsNoTracking()
+                                             join f in _context.Follows
+                                             on u.Id equals f.FollowerId
+                                             where f.FollowingId == id
+                                             select new Follower
+                                             {
+                                                 Id = u.Id,
+                                                 Username = u.Username
+                                             }).ToListAsync();
+
+        return followers;
+
+    }
+}
