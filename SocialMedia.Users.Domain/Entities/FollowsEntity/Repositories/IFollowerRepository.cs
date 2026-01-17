8443f52c83fcb894131fe543d81d17d1a3186d0c
Joovs <jovannaescogar9@gmail.com>
2026-01-16 20:36:34 -0600

    add: interface IFollowerRepository

---
+++ SocialMedia.Users.Domain/Entities/FollowsEntity/Repositories/IFollowerRepository.cs
@@ -0,0 +1,9 @@
+﻿using SocialMedia.Users.Domain.Entities.FollowsEntity.Models.SeeFollowers;
+
+namespace SocialMedia.Users.Domain.Entities.FollowsEntity.Repositories;
+
+public interface IFollowerRepository
+{
+    public Task<bool> UserExists(Guid id, CancellationToken cancellationToken);
+    public Task<List<Follower>> SeeFollowers(Guid id, CancellationToken cancellationToken);
+}
