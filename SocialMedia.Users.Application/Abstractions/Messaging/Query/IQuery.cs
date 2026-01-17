344ae11791130dfdbf2672be3e118901508c4e01
Joovs <jovannaescogar9@gmail.com>
2026-01-16 18:50:01 -0600

    add: Interfaces that are implemented in queries

---
+++ SocialMedia.Users.Application/Abstractions/Messaging/Query/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using SocialMedia.Users.Application.Shared;
+
+namespace SocialMedia.Users.Application.Abstractions.Messaging.Query;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+}
---
+++ SocialMedia.Users.Application/Abstractions/Messaging/Query/IQueryHandler.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using SocialMedia.Users.Application.Shared;
+
+namespace SocialMedia.Users.Application.Abstractions.Messaging.Query;
+
+public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>
+{
+}
