9c28d0ba421a77c04849795b9e886712213f728a
Joovs <jovannaescogar9@gmail.com>
2026-01-19 00:45:41 -0600

    add: unit tests

---
+++ SocialMedia.Users.Test/Follows/Mocks/FollowMockRepository.cs
@@ -0,0 +1,46 @@
+﻿using SocialMedia.Users.Domain.Entities.FollowsEntity.Models.SeeFollowers;
+using SocialMedia.Users.Domain.Entities.FollowsEntity.Repositories;
+
+namespace SocialMedia.Users.Test.Follows.Mocks;
+
+public class FollowMockRepository : IFollowRepository
+{
+    private readonly List<Guid> _database = new()
+    {
+        new Guid("6FBB9F92-3FD1-4C8A-747A-08DE53C6BA21"),
+        new Guid("6FBB9F92-3FD1-4C8A-747A-08DE53C6BA22"),
+        new Guid("6FBB9F92-3FD1-4C8A-747A-08DE53C6BA23")
+    };
+
+    public Task<List<Follower>> SeeFollowers(Guid id, CancellationToken cancellationToken)
+    {
+        List<Follower> response = new List<Follower>()
+        {
+            new Follower
+            {
+                Id = new Guid("6FBB9F92-3FD1-4C8A-747A-08DE53C6BA24"),
+                Username = "Username4"
+            },
+            new Follower
+            {
+                Id = new Guid("6FBB9F92-3FD1-4C8A-747A-08DE53C6BA24"),
+                Username = "Username4"
+            }
+        };
+
+            return Task.FromResult(response);
+    }
+
+    public Task<bool> UserExists(Guid id, CancellationToken cancellationToken)
+    {
+        bool exists = _database.Any(u =>
+            u == id);
+
+        if (!exists)
+        {
+            return Task.FromResult(false);
+        }
+
+        return Task.FromResult(true);
+    }
+}
---
+++ SocialMedia.Users.Test/Follows/Queries/SeeFollowersQuery/SeeFollowersQueryHandlerTest.cs
@@ -0,0 +1,47 @@
+﻿using SocialMedia.Users.Domain.Entities.FollowsEntity.Models.SeeFollowers;
+using SocialMedia.Users.Test.Follows.Mocks;
+using System;
+
+namespace SocialMedia.Users.Test.Follows.Queries.SeeFollowersQuery;
+
+public class SeeFollowersQueryHandlerTest
+{
+    private readonly FollowMockRepository _repository = new FollowMockRepository();
+    private readonly CancellationToken cancellationToken = new CancellationToken();
+
+    //Happy path
+    [Theory]
+    [InlineData("6FBB9F92-3FD1-4C8A-747A-08DE53C6BA21")]
+    public async Task handleShouldReturnAListOfFollowers(Guid userID)
+    {
+        //Arrange 
+        if(userID == Guid.Empty) throw new ArgumentNullException(nameof(userID));
+
+        bool userExists = await _repository.UserExists(userID, cancellationToken);
+        
+        if(!userExists) throw new ArgumentException(nameof(userID));
+
+        //Act
+        List<Follower> response = await _repository.SeeFollowers(userID, cancellationToken);
+
+        //Assert
+        Assert.NotNull(response);
+        Assert.IsType<List<Follower>>(response);
+
+    }
+    
+    [Theory]
+    [InlineData("6FBB9F92-3FD1-4C8A-747A-08DE53C6BA25")]
+    public async Task handleShouldReturnArgumenExcepcionWhenUserIdDoesNotExists(Guid userID)
+    {
+        //Arrange 
+
+        //Act
+        var result = await _repository.UserExists(userID, CancellationToken.None);
+
+        //Assert
+        Assert.False(result);
+        
+
+    }
+}
