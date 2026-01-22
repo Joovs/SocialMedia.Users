33fd74bc1d1387fde29146a0ce9864d0f6cabc6c
Garcia <luis.garcia@capgemini.com>
2026-01-22 15:39:11 -0600

    fix - Refactor injection about services

--- SocialMedia.Users.Infrastructure/InjectionManagers/RepositoriesManager.cs
+++ SocialMedia.Users.Infrastructure/InjectionManagers/RepositoriesManager.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SocialMedia.Users.Application.Repositories;
-using SocialMedia.Users.Domain.Services.JwtServices;
-using SocialMedia.Users.Domain.Services.PasswordHashes;
 using SocialMedia.Users.Infrastructure.Persistence.Repositories;
-using SocialMedia.Users.Infrastructure.Services;
-using SocialMedia.Users.Infrastructure.Services.PasswordHasher;
 
 namespace SocialMedia.Users.Infrastructure.InjectionManagers;
 
@@ -13,8 +9,6 @@ public static class RepositoriesManager
     public static IServiceCollection AddRepositories(this IServiceCollection services)
     {
         services.AddScoped<IUserRepository, UserRepository>();
-        services.AddScoped<IJwtService, JwtService>();
-        services.AddScoped<IPasswordHasher, PasswordHasher>();
 
         return services;
     }
